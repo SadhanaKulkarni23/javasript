70ba97f1de6670fa69cd261a82751044126ce306
SadhanaKulkarni23 <sadhanamkulkarni23@gmail.com>
2023-05-06 23:11:19 +0530

    hello.js
    hello details

---
+++ hello.js
@@ -0,0 +1,14 @@
+console.log("Sadhana Kulkarni..");
+console.log(" learning JS.");
+console.log("Sajjala Kulkarni..");
+var firstName = "Sadhana";
+console.log(firstName);
+var age; //variable declaration
+age = "24"; //variable Initialization
+console.log(age);
+var holder;
+console.log(holder); //undefined
+holder = "My Spec";
+console.log(holder);
+holder = "Sadhana Spec"; //variable update
+console.log(holder);
